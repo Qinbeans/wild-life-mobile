06538d38efc07f0101700f703e91413cacf02205
David Coles <davidepcoles@gmail.com>
2022-11-15 14:05:56 -0800

    Added ui elements

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import './ml/process.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 void main() {
+  //initModel();
   runApp(const MyApp());
 }
 
@@ -12,40 +14,42 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: const Color.fromARGB(255, 37, 37, 37),
-          centerTitle: true,
-          actions: [
-            IconButton(
-              icon: const FaIcon(FontAwesomeIcons.leaf),
-              onPressed: () {},
-            ),
-          ],
-          title: const Text('Wildlife'),
-        ),
-        body: const Center(
-          child: Text('Hello World'),
-        ),
-      ),
+      theme: ThemeData(
+          scaffoldBackgroundColor: const Color.fromARGB(255, 37, 37, 37),
+          fontFamily: 'Roboto'),
+      home: const MyHomePage(),
     );
   }
 }
 
-// class MyHomePage extends StatefulWidget {
-//   const MyHomePage({super.key, required this.title});
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
 
-//   // This widget is the home page of your application. It is stateful, meaning
-//   // that it has a State object (defined below) that contains fields that affect
-//   // how it looks.
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
 
-//   // This class is the configuration for the state. It holds the values (in this
-//   // case the title) provided by the parent (in this case the App widget) and
-//   // used by the build method of the State. Fields in a Widget subclass are
-//   // always marked "final".
-
-//   final String title;
-
-//   @override
-//   State<MyHomePage> createState() => _MyHomePageState();
-// }
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          backgroundColor: const Color.fromARGB(255, 37, 37, 37),
+          appBar: AppBar(
+            backgroundColor: const Color.fromARGB(255, 37, 37, 37),
+            centerTitle: true,
+            actions: [
+              IconButton(
+                icon: const FaIcon(FontAwesomeIcons.leaf),
+                color: Colors.green,
+                onPressed: () {},
+              ),
+            ],
+            title: const Text('Wildlife'),
+          ),
+          body: Container(
+              child: Column(
+                  children: [ElevatedButton(onPressed: null, child: null)]))),
+    );
+  }
+}
