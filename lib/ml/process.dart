1856495949e30a5b19f9195fb4cabc277d5d8365
Qinbeans <rybeats@live.com>
2022-12-12 19:07:20 -0800

    revert back to flutter tflite to get results

--- lib/ml/process.dart
+++ lib/ml/process.dart
@@ -1,64 +1,149 @@
+import 'dart:math';
 import 'dart:developer' as developer;
+import 'dart:io';
+import 'package:tflite_flutter/tflite_flutter.dart';
+import 'package:tflite_flutter_helper/tflite_flutter_helper.dart';
 
-import 'package:flutter/foundation.dart';
 import 'package:wild_life_mobile/ml/detection.dart';
 
-import 'package:image/image.dart';
-import 'dart:io';
-import 'package:flutter_vision/flutter_vision.dart';
+Classifier? classifier;
 
-Processor? classifier;
+class Classifier {
+  late Interpreter? _interpreter;
+  Interpreter? get interpreter => _interpreter;
 
-class Processor {
-  FlutterVision classifier = FlutterVision();
-  String modelDir = "";
-  String labelDir = "";
-  final threadCount = 4;
-  final threshold = 0.7;
-  final imageSize = 512;
-  //constructor
-  Processor({required this.modelDir, required this.labelDir});
+  /// image size into interpreter
+  static const int inputSize = 512;
 
-  void dispose() async {
-    await classifier.closeYoloModel();
-  }
+  ImageProcessor? imageProcessor;
+  late List<List<int>> _outputShapes;
+  late List<TfLiteType> _outputTypes;
+  late List<String> _labels;
 
-  //load model
-  Future<void> loadModel() async {
-    final response = await classifier.loadYoloModel(
-        modelPath: modelDir,
-        labels: labelDir,
-        numThreads: threadCount,
-        useGpu: false);
-    if (response.type == "success") {
-      developer.log("Model loaded successfully");
-    } else {
-      developer.log("Model failed to load");
+  static const double objConfTh = 0.80;
+  static const double clsConfTh = 0.80;
+
+  late int spacing;
+  late int clsNum;
+
+  /// load interpreter
+  Future<void> loadModel(String modelFileName) async {
+    try {
+      _interpreter = await Interpreter.fromAsset(
+        modelFileName,
+        options: InterpreterOptions()..threads = 4,
+      );
+      final outputTensors = _interpreter!.getOutputTensors();
+      _outputShapes = [];
+      _outputTypes = [];
+      for (final tensor in outputTensors) {
+        _outputShapes.add(tensor.shape);
+        _outputTypes.add(tensor.type);
+      }
+      //[0][1][2] = [1, 16320, 9]
+      spacing = _outputShapes[0][2] - 1;
+    } on Exception catch (e) {
+      developer.log(e.toString());
     }
   }
 
-  List<Uint8List> formatImage(File image) {
-    var input = decodeImage(image.readAsBytesSync())!;
-    return [input.getBytes()];
+  void loadLabels(String labelFileName) async {
+    try {
+      _labels = await FileUtil.loadLabels("assets/$labelFileName");
+      clsNum = _labels.length;
+    } catch (e) {
+      developer.log(e.toString());
+    }
   }
 
-  //detect objects
-  Future<List<Detection>> forward(File input) async {
-    final image = formatImage(input);
-    //format image
-    final response = await classifier.yoloOnFrame(
-        bytesList: image,
-        imageHeight: imageSize,
-        imageWidth: imageSize,
-        confThreshold: threshold);
-    if (response.type == "error") {
-      //developer.log("Error: ${response.message}");
+  void load(String modelFileName, String labelFileName) async {
+    await loadModel(modelFileName);
+    loadLabels(labelFileName);
+  }
+
+  void printOutputShapes() {
+    for (var i = 0; i < _outputShapes.length; i++) {
+      developer.log("Shape: ${_outputShapes[i].toString()}");
+    }
+  }
+
+  /// image pre process
+  TensorImage getProcessedImage(TensorImage inputImage) {
+    final padSize = max(inputImage.height, inputImage.width);
+
+    imageProcessor ??= ImageProcessorBuilder()
+        .add(
+          ResizeWithCropOrPadOp(
+            padSize,
+            padSize,
+          ),
+        )
+        .add(
+          ResizeOp(
+            inputSize,
+            inputSize,
+            ResizeMethod.BILINEAR,
+          ),
+        )
+        .build();
+    return imageProcessor!.process(inputImage);
+  }
+
+  List<Detection> predict(File image) {
+    printOutputShapes();
+    if (_interpreter == null) {
       return [];
     }
-    //get detections
-    final rawDetections = response.data as List<Map<String, dynamic>>;
-    //convert to Detection objects
-    final detections = rawDetections.map((e) => Detection.fromMap(e)).toList();
-    return detections;
+
+    var inputImage = TensorImage.fromFile(image);
+    inputImage = getProcessedImage(inputImage);
+
+    ///  normalize from zero to one
+    List<double> normalizedInputImage = [];
+    for (var pixel in inputImage.tensorBuffer.getDoubleList()) {
+      normalizedInputImage.add(pixel / 255.0);
+    }
+    var normalizedTensorBuffer = TensorBuffer.createDynamic(TfLiteType.float32);
+    normalizedTensorBuffer
+        .loadList(normalizedInputImage, shape: [inputSize, inputSize, 3]);
+
+    final inputs = [normalizedTensorBuffer.buffer];
+
+    /// tensor for results of inference
+    final outputLocations = TensorBufferFloat(_outputShapes[0]);
+    final outputs = {
+      0: outputLocations.buffer,
+    };
+
+    _interpreter!.runForMultipleInputs(inputs, outputs);
+
+    /// make recognition
+    final recognitions = <Detection>[];
+    List<double> outputRes = outputLocations.getDoubleList();
+    for (var i = 0; i < outputRes.length; i += 5 * clsNum) {
+      //8 bit int to double maintaining bits
+      var conf = outputRes[i + 4];
+      // developer.log("conf: $conf");
+      if (conf < objConfTh || conf > 1) continue;
+      var maxConf = outputRes.sublist(i + 5, i + 5 + clsNum - 1).reduce(max);
+      //developer.log(maxConf.toString());
+      if (maxConf < clsConfTh) continue;
+      var cls = outputRes.sublist(i + 5, i + 5 + clsNum - 1).indexOf(maxConf) %
+          clsNum;
+      var box = Box(
+        outputRes[i] * inputSize,
+        outputRes[i + 1] * inputSize,
+        outputRes[i + 2] * inputSize,
+        outputRes[i + 3] * inputSize,
+      );
+      recognitions.add(
+        Detection(
+          box,
+          conf,
+          _labels[cls],
+        ),
+      );
+    }
+    return recognitions;
   }
 }
